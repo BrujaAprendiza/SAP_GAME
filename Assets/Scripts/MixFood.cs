ac28dc3bbe26c70cff0c94050bd93b9023966a11
JoniLagostinoh <62272163+JoniLagostinoh@users.noreply.github.com>
2026-05-07 19:35:09 +0100

    PC Crash, safety commit
    Pc crashed, i'll commit rn

--- Assets/Scripts/MixFood.cs
+++ Assets/Scripts/MixFood.cs
@@ -1,16 +1,51 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class MixFood : MonoBehaviour
+public class MixFood : MonoBehaviour, IBeginDragHandler, IEndDragHandler
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] private List<Transform> waypoints;  // Drag your transforms here
+    [SerializeField] private float moveSpeed = 5f;       // Units per second
+    [SerializeField] private float arrivalDistance = 0.05f; // How close to consider "reached"
+
+    private bool isMoving = false;
+    private int currentWaypointIndex = 0;
+
     void Update()
     {
-        
+        // Any mouse click or touch tap
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!isMoving)
+            {
+                StartCoroutine(MoveThroughWaypoints());
+            }
+        }
+    }
+
+    IEnumerator MoveThroughWaypoints()
+    {
+        isMoving = true;
+        currentWaypointIndex = 0;
+
+        while (currentWaypointIndex < waypoints.Count)
+        {
+            Transform target = waypoints[currentWaypointIndex];
+
+            // Move towards the target
+            while (Vector3.Distance(transform.position, target.position) > arrivalDistance)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                yield return null;
+            }
+
+            // Snap to exact position (optional)
+            transform.position = target.position;
+
+            currentWaypointIndex++;
+        }
+
+        isMoving = false;
     }
 }
